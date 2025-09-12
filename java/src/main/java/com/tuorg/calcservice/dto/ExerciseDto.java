328e6e5087038fe39b2a0536cb5d2cd906441474
Jefferson236 <62144310+Jefferson236@users.noreply.github.com>
2025-09-12 07:57:55 -0400

    no

--- java/src/main/java/com/tuorg/calcservice/dto/ExerciseDto.java
+++ java/src/main/java/com/tuorg/calcservice/dto/ExerciseDto.java
@@ -1,6 +1,7 @@
 package com.tuorg.calcservice.dto;
 
 public class ExerciseDto {
+    // aceptar strings (slugs) o números -> usar String es más flexible
     public Long id;
     public String slug;
     public String name;
